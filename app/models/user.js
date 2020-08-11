fad5af60e1d0e5eba5dfb448d10c04afeb3cecda
Dave-92-stack <dave.sher92@gmail.com>
2020-08-11 12:11:58 -0400

    Edited user schema

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  experience: {
+    type: Number,
+    required: true
   },
   hashedPassword: {
     type: String,
@@ -12,45 +21,12 @@ const userSchema = new mongoose.Schema({
   token: String
 }, {
   timestamps: true,
-  toObject: {
-    // remove `hashedPassword` field when we call `.toObject`
+  toJSON: {
     transform: (_doc, user) => {
       delete user.hashedPassword
       return user
     }
   }
 })
-module.exports = mongoose.model('User', userSchema)
 
-// const mongoose = require('mongoose')
-//
-// const userSchema = new mongoose.Schema({
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true
-//   },
-//   name: {
-//     type: String,
-//     required: true
-//   },
-//   experience: {
-//     type: Number,
-//     required: true
-//   },
-//   hashedPassword: {
-//     type: String,
-//     required: true
-//   },
-//   token: String
-// }, {
-//   timestamps: true,
-//   toJSON: {
-//     transform: (_doc, user) => {
-//       delete user.hashedPassword
-//       return user
-//     }
-//   }
-// })
-//
-// module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema)
