2c897729741dc45a717b3867fe7c19936ba3cad0
Dave-92-stack <dave.sher92@gmail.com>
2020-08-17 13:15:39 -0400

    Quick edit in curlscripts

--- curl-scripts/advBooks/destroy.sh
+++ curl-scripts/advBooks/destroy.sh
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 curl "https://localhost:4741/advbooks/${ID}" \
 --include \
 --request DELETE \
 --header "Authorization: Bearer ${TOKEN}" \
+=======
+curl "http://localhost:4741/advbooks/${ID}" \
+  --include \
+  --request DELETE \
+  --header "Authorization: Bearer ${TOKEN}" \
+>>>>>>> Quick edit in curlscripts
 
 echo
